6a50a02d41d610da30f0abc6dbbe429f53057916
CapsAdmin <eliashogstvedt@gmail.com>
2021-07-11 14:36:16 +0200

    start implementing ui for input

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,116 @@
-import { useRef } from "react"
+import { useEffect, useRef, useState } from "react"
 import { Simulation } from "./Simulation"
+import exampleVideos from "./assets/videos.json"
+
+let VIDEO_CONNECTED_TO_AUDIO = false
+
+let audioContext = null as unknown as AudioContext
+let audioAnalyser = null as unknown as AnalyserNode
+let fftArray = null as unknown as Float32Array
+
+const ensureAudioContext = () => {
+	if (audioContext) return
+
+	audioContext = new AudioContext({
+		latencyHint: "interactive",
+	})
+
+	audioAnalyser = audioContext.createAnalyser()
+	audioAnalyser.smoothingTimeConstant = 0.0
+	audioAnalyser.fftSize = 2048
+
+	fftArray = new Float32Array(audioAnalyser.frequencyBinCount)
+}
+
+const audioSources = new Map<string, MediaStreamAudioSourceNode>()
+let currentVideoSource = exampleVideos[0].source
+
+const removeAudioSource = async (deviceId: string) => {
+	const source = audioSources.get(deviceId)
+	if (!source) return
+
+	for (const track of source.mediaStream.getTracks()) {
+		track.stop()
+	}
+	source.disconnect()
+
+	audioSources.delete(deviceId)
+}
+
+const addAudioSource = async (deviceId: string) => {
+	const stream = await navigator.mediaDevices.getUserMedia({
+		video: false,
+		audio: {
+			deviceId: deviceId,
+			echoCancellation: false,
+			noiseSuppression: false,
+			autoGainControl: false,
+			channelCount: 2,
+			latency: 0.02,
+		},
+	})
+
+	const source = audioContext.createMediaStreamSource(stream)
+
+	source.connect(audioAnalyser)
+	source.connect(audioContext.destination)
+
+	audioSources.set(deviceId, source)
+
+	console.log("started listening to " + deviceId)
+}
+
+const setVideoSource = async (source: string) => {
+	const video = document.getElementById("videoPlayer") as HTMLVideoElement
+	if (!video) return
+
+	if (source.startsWith("http")) {
+		video.src = source
+	} else {
+		video.srcObject = await navigator.mediaDevices.getUserMedia({
+			video: {
+				deviceId: source,
+			},
+			audio: false,
+		})
+	}
+
+	currentVideoSource = source
+}
+
+const getVideoSource = async () => {
+	return currentVideoSource
+}
 
 function App() {
 	const simRef = useRef<Simulation | null>(null)
-	const videoRef = useRef<HTMLVideoElement | null>(null)
+	const [devices, setDevices] = useState<MediaDeviceInfo[]>([])
+	const [, render] = useState({})
+
+	useEffect(() => {
+		;(async () => {
+			setDevices(await navigator.mediaDevices.enumerateDevices())
+		})()
+	}, [])
+
 	return (
 		<div className="App">
 			<Simulation
 				onLoad={(sim) => {
 					simRef.current = sim
+
+					const video = document.getElementById("videoPlayer") as HTMLVideoElement
+					if (!video) return
+
+					sim.PreDraw = () => {
+						audioAnalyser.getFloatTimeDomainData(fftArray) // move me out
+
+						sim.UpdateFFT(fftArray)
+
+						if (video.readyState >= 2) {
+							sim.UpdateTexture(video)
+						}
+					}
 				}}
 				simulation={`
                     #define FFT(f) texture(fft_tex, vec2(f, 0.0)).x
@@ -61,16 +163,72 @@ function App() {
                     `}
 			></Simulation>
 
-			<video controls src="https://test-videos.co.uk/vids/bigbuckbunny/mp4/h264/1080/Big_Buck_Bunny_1080_10s_30MB.mp4" ref={videoRef}></video>
+			<div>
+				<h2>audio inputs</h2>
+				<ul>
+					{devices.map((info) => {
+						if (info.kind === "audioinput") {
+							return (
+								<li key={info.deviceId}>
+									<input
+										title={info.label}
+										type="checkbox"
+										checked={audioSources.has(info.deviceId)}
+										onChange={async (e) => {
+											ensureAudioContext()
+											if (audioSources.has(info.deviceId)) {
+												await removeAudioSource(info.deviceId)
+											} else {
+												await addAudioSource(info.deviceId)
+											}
+											render({})
+										}}
+									/>
+									<span>{info.label}</span>
+								</li>
+							)
+						}
+						return null
+					})}
+				</ul>
+			</div>
+
+			<div>
+				<h2>video input</h2>
+				<ul>
+					<select
+						value={currentVideoSource}
+						onChange={(e) => {
+							ensureAudioContext()
+							setVideoSource(e.currentTarget.value)
+							render({})
+						}}
+					>
+						{devices.map((info) => {
+							if (info.kind === "videoinput") {
+								return (
+									<option key={info.deviceId} value={info.deviceId}>
+										{info.label}
+									</option>
+								)
+							}
+							return null
+						})}
+						{exampleVideos.map((info) => {
+							return (
+								<option key={info.source} value={info.source}>
+									{info.title}
+								</option>
+							)
+						})}
+					</select>
+				</ul>
+			</div>
 
 			<button
 				onClick={async () => {
-					const video = videoRef.current
+					const video = document.getElementById("videoPlayer") as HTMLVideoElement
 					if (!video) return
-
-					const sim = simRef.current
-					if (!sim) return
-
 					const desktopStream = await (navigator.mediaDevices as MediaDevices & { getDisplayMedia: (opts: MediaStreamConstraints) => MediaStream }).getDisplayMedia({
 						video: true,
 						audio: {
@@ -82,69 +240,44 @@ function App() {
 						},
 					})
 
-					console.log(await navigator.mediaDevices.enumerateDevices())
-
 					const videoStream = desktopStream
-					/*				
-                    const videoStream = await navigator.mediaDevices.getUserMedia({
-                        video: true,
-                        audio: false,
-                    })
-                    */
-					const audioStream = await navigator.mediaDevices.getUserMedia({
-						video: false,
-						audio: {
-							echoCancellation: false,
-							noiseSuppression: false,
-							autoGainControl: false,
-							channelCount: 2,
-							latency: 0.02,
-						},
-					})
 
-					let context = new AudioContext({
-						latencyHint: "interactive",
-					})
-
-					const desktopSource = context.createMediaStreamSource(videoStream)
-					const voiceSource = context.createMediaStreamSource(audioStream)
-
-					let analyser = context.createAnalyser()
-					//analyser.minDecibels = -90
-					//analyser.maxDecibels = -10
-					analyser.smoothingTimeConstant = 0.0
-					analyser.fftSize = 2048
-
-					var bufferLength = analyser.frequencyBinCount
-					const fftArray = new Float32Array(bufferLength)
-
-					let gain = context.createGain()
-					gain.gain.value = 1.25
-					desktopSource.connect(gain)
-
-					gain.connect(analyser)
-					voiceSource.connect(analyser)
-
-					desktopSource.connect(context.destination)
-					voiceSource.connect(context.destination)
+					const desktopSource = audioContext.createMediaStreamSource(videoStream)
+					desktopSource.connect(audioAnalyser)
+					desktopSource.connect(audioContext.destination)
 
 					const stream = new MediaStream([...videoStream.getVideoTracks()])
 
 					video.srcObject = stream
-
-					sim.PreDraw = () => {
-						analyser.getFloatTimeDomainData(fftArray) // move me out
-
-						sim.UpdateFFT(fftArray)
-
-						if (video.readyState >= 2) {
-							sim.UpdateTexture(video)
-						}
-					}
 				}}
 			>
-				start capture
+				Desktop
 			</button>
+
+			<div>
+				<h2>video player</h2>
+				<video
+					key="videoplayer"
+					ref={(e) => {
+						if (!e) return
+						setVideoSource(currentVideoSource)
+
+						if (!VIDEO_CONNECTED_TO_AUDIO) {
+							ensureAudioContext()
+							const audioSource = audioContext.createMediaElementSource(e)
+
+							audioSource.connect(audioAnalyser)
+							audioSource.connect(audioContext.destination)
+							VIDEO_CONNECTED_TO_AUDIO = true
+						}
+					}}
+					id="videoPlayer"
+					style={{ width: 512 }}
+					controls
+					crossOrigin="anonymous"
+					src=""
+				/>
+			</div>
 		</div>
 	)
 }
--- src/Simulation.tsx
+++ src/Simulation.tsx
@@ -172,7 +172,7 @@ export class Simulation extends GLRenderer {
 		}
 
 		gl.bindTexture(gl.TEXTURE_2D, this.videoTexture)
-		gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, video)
+		gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGB, gl.RGB, gl.UNSIGNED_BYTE, video)
 	}
 
 	UpdateFFT(array: Float32Array) {
